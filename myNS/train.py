6d896c679314bb15ef6ea165dd18c4df1397ded5
zhy <993429967@qq.com>
2020-04-22 18:25:44 +0800

    add

--- myNS/train.py
+++ myNS/train.py
@@ -8,6 +8,30 @@ loss_modle_file="../pretrained/vgg_16.ckpt"
 style_image = "img/wave.jpg"
 image_size = 256
 
+# list
+style_layers = ["vgg_16/conv1/conv1_2",
+                "vgg_16/conv2/conv2_2",
+                "vgg_16/conv3/conv3_3",
+                "vgg_16/conv4/conv4_3"]
+
+
+def gram(layer):  # gram矩阵实现  
+    shape = tf.shape(layer)
+    sh    = layer.shape 
+    num_images = shape[0]
+    width = shape[1]
+    height = shape[2]
+    num_filters = shape[3]
+    #print num_images,sh,width,height,num_filters
+    # stack 矩阵拼接
+    print tf.stack([num_images, -1, num_filters])
+    filters = tf.reshape(layer, tf.stack([num_images, -1, num_filters]))  #把layer的w h合并
+    #print "reshp" , filters.shape
+    grams = tf.matmul(filters, filters, transpose_a=True) / tf.to_float(width * height * num_filters)
+
+    return grams
+
+
 # 读入风格图片 并处理 输出制定中间层的数据
 def get_style_features():
     with tf.Graph().as_default():
@@ -23,10 +47,21 @@ def get_style_features():
     # 256 256 3 -> 0 256 256 3 在最前面增加一个维度
     images = tf.expand_dims(image_preprocessing_fn(image,size,size),0)
 
+    # 得到风格图像各层网络中的矩阵
     _, endpoints_dict = network_fn(images,spatial_squeeze=False)
-    # print(endpoints_dict.size())
-    for k,v in endpoints_dict.items():
-        print k,"aaa" ,v
+    # 测试endpoints_dict
+    #for k,v in endpoints_dict.items():
+    #    print k,"---" ,v
+
+    features = []
+    for layer in style_layers:
+        feature = endpoints_dict[layer]
+        #print gram(features)
+        feature = tf.squeeze(gram(feature),[0])  # 变成2维
+        # print features
+        features.append(feature)
+    with tf.Session() as sess:
+        init_func = 
 
 # 读入coco数据集
 
